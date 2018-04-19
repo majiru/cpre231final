1877383a64cffb1c766531c7fef480975413bd54
Jacob Moody <sinful.spartan@gmail.com>
2018-04-19 01:17:03 -0500

    Fixed php injection

--- action.php
+++ action.php
@@ -1,5 +1,5 @@
 <?php
-$file = 'comments.txt';
+$file = '../comments.txt';
 if(isset($_POST['field1']) && isset($_POST['field2'])) {
 	$fh = fopen($file,"a+");
 	$string = $_POST['field1'].': '.$_POST['field2']."\r\n\r\n";
